21333d257f0e5f9ddad416bb1c3ee01f0db76542
BikashPandeya <bikashpandeya5@gmail.com>
2025-04-20 20:51:46 +0545

    Made protectedRoute middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -8,9 +8,18 @@ export const protectRoute = async (req, res, next) => {
             return res.status(401).json({message : "Unauthorized - No token provided"})
         }
         const decoded = jwt.verify(token , process.env.JWT_SECRET)
-        req.user = await User.findById(decoded.userId).select("-password")
+        if(!decoded){
+            return res.status(401).json({message : "Unauthorized - Invalid token"})
+        }
+        const user = await User.findById(decoded.userId).select("-password");
+        if(!user){
+            return res.status(404).json({message : "Unauthorized - User not found"})
+        }
+
+        req.user = user;
         next()
     } catch (error) {
-        
+        console.log("Error in protectRoute middleware" , error.message)
+        res.status(500).json({message : "Internal Server Error"})
     }
 }
