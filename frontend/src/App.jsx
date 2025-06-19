027b591aac255e099afeb43aa587b89e5a40a4b1
BikashPandeya <bikashpandeya5@gmail.com>
2025-06-19 19:30:04 +0545

    Adding Routing in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import SignUpPage from "./pages/SignUpPage.jsx";
 import Login from "./pages/LoginPage.jsx";
 import SettingsPage from "./pages/SettingsPage.jsx";  
 import ProfilePage from "./pages/ProfilePage.jsx";
+import { Routes, Route } from "react-router-dom";
+
 
 const App = () => {
   return (
