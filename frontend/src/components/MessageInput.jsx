790a2c8de3cb26dc547ad6fee80be3a3fc6dd19d
BikashPandeya <bikashpandeya5@gmail.com>
2025-06-24 21:26:34 +0545

    chat container

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import { useState } from 'react'
+
 
 const MessageInput = () => {
+    const [text, setText] = useState("")
+    const [imagePreview, setImagePreview] = useState(null)
   return (
     <div>MessageInput</div>
   )
