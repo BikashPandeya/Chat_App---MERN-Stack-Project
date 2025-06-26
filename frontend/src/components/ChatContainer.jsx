5c075c1f6ccbc245d1904b6275d36134f2ea5e60
BikashPandeya <bikashpandeya5@gmail.com>
2025-06-26 21:02:13 +0545

    Making messages field

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,31 +1,63 @@
-import { useChatStore } from "../store/useChatStore"
-import { useEffect } from "react"
-import ChatHeader from "./ChatHeader"
-import MessageInput from "./MessageInput"
-import MessageSkeleton from "./skeletons/MessageSkeleton"
+import { useChatStore } from "../store/useChatStore";
+import { useAuthStore } from "../store/useAuthStore";
+import { useEffect } from "react";
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
+
 
 const ChatContainer = () => {
-  const {messages , getMessages , isMessageLoading ,selectedUser}  = useChatStore()
+  const { messages, getMessages, isMessageLoading, selectedUser } =
+    useChatStore();
+  const { authUser } = useAuthStore();
   useEffect(() => {
-    getMessages(selectedUser._id)
-  }, [selectedUser._id , getMessages])
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages]);
 
-  if(isMessageLoading) return (
-    <div className="flex-1 flex flex-col overflow-auto">
-      <ChatHeader/>
-      <MessageSkeleton />
-      <MessageInput />
-    </div>
-  )
+  if (isMessageLoading)
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
+    );
 
-  
   return (
     <div className="flex-1 flex flex-col overflow-auto">
-      <ChatHeader/>
-      <p>Messages</p>
+      <ChatHeader />
+      <div className="flex-1 flex flex-col overflow-y-auto p-4">
+        {messages.map((message) => (
+          <div
+            key={message._id}
+            className={`chat ${
+              message.senderId == authUser._id ? "chat-end" : "chat-start"
+            } `}
+          >
+            <div className="chat-image avatar">
+              <div className="size-10 rounded-full border">
+                <img
+                  src={
+                    message.senderId === authUser._id
+                      ? authUser.profilePic || "/avatar.png"
+                      : selectedUser.profilePic || "/avatar.png"
+                  }
+                  alt="Profile Pic"
+                />
+              </div>
+            </div>
+            <div className="chat-header mb-1">
+              <time className="text-xs opacity-50 ml-1">
+                {message.createdAt}
+              </time>
+            </div>
+            {/* Add message text/image here if needed */}
+          </div>
+        ))}
+      </div>
       <MessageInput />
     </div>
-  )
-}
+  );
+};
 
-export default ChatContainer
+export default ChatContainer;
