42d479160ef2c78c9a9056d825fd27ad97834831
BikashPandeya <bikashpandeya5@gmail.com>
2025-07-10 19:13:16 +0545

    FInalyzing the application before deploying

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -7,9 +7,14 @@ import { Users } from "lucide-react";
 const Sidebar = () => {
   const { getUsers, users, selectedUser, isUsersLoading, setSelectedUser } = useChatStore();
   const { onlineUsers } = useAuthStore();
+  const [showOnlineOnly, setShowOnlineOnly] = useState(false);
   useEffect(() => {
     getUsers();
   }, [getUsers]);
+    const filteredUsers = showOnlineOnly
+    ? users.filter((user) => onlineUsers.includes(user._id))
+    : users;
+
 
   if (isUsersLoading) return <SidebarSkeleton />;
   return (
@@ -20,10 +25,30 @@ const Sidebar = () => {
           <span className="font-medium hidden lg:block">Contacts</span>
         </div>
         {/* TODO: Online filter toggle */}
+                <div className="mt-3 flex items-center gap-1 w-full justify-start -ml-2">
+          <label className="cursor-pointer flex items-center gap-1 ml-0">
+            <input
+              type="checkbox"
+              checked={showOnlineOnly}
+              onChange={(e) => setShowOnlineOnly(e.target.checked)}
+              className="checkbox checkbox-sm"
+            />
+            <span className="text-xs lg:hidden">
+              Show Online Users ({Math.max(onlineUsers.length - 1, 0)})
+            </span>
+            <span className="text-xs lg:text-sm hidden lg:inline">
+              Show online only
+            </span>
+          </label>
+          <span className="text-xs text-zinc-500 hidden lg:inline">
+            ({onlineUsers.length - 1} online)
+          </span>
+        </div>
+      
       </div>
 
       <div className="overflow-y-auto w-full py-3">
-        {users.map((user) => (
+        {filteredUsers.map((user) => (
           <button
             key={user._id}
             onClick={() => setSelectedUser(user)}
@@ -60,6 +85,13 @@ const Sidebar = () => {
             </div>
           </button>
         ))}
+        {
+          filteredUsers.length === 0 && (
+            <div className="text-center text-zinc-500 mt-5">
+              No Online Users
+            </div>
+          )
+        }
       </div>
     </aside>
   );
